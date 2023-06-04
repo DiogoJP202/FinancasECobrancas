d02de89160627b2f935bc5f6842c18eb1e312c2b
Diogo Antonny <Diogojp202@gmail.com>
2023-06-03 21:41:04 -0300

    Projeto Financeiro iniciado.
    - html da pagina inicial concluído;
    - css da pagina inicial concluído;
    - javascript da pagina inicial concluído;
    - html da pagina de Auxíliar finaceiro em andamento;

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('mouseover', (event) => {
+    element = event.target;
+    if(element.classList.contains('linkPort')){
+        const emoji = document.querySelector('#emoji');
+        const background = document.querySelector('nav');
+
+        background.style.backgroundImage = "url(https://img1.picmix.com/output/stamp/normal/1/0/3/2/832301_14b4a.gif)";
+        emoji.style.display = 'block';
+    }
+});
+
+document.addEventListener('mouseout', (event) => {
+    element = event.target;
+    if(element.classList.contains('linkPort')){
+        const emoji = document.querySelector('#emoji');
+        const background = document.querySelector('nav');
+
+        background.style.backgroundImage = "";
+        emoji.style.display = 'none';
+    }
+});
